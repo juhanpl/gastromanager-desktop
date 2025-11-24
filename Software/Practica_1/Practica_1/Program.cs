2282e12bf580c09894e77dae79913d36c00d8a3b
Juan <mendozalopez.py@gmail.com>
2025-11-24 14:53:25 -0500

    fix: eliminar carpetas innecesarias

--- Software/Practica_1/Practica_1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿//Solicita un número entero N y muestra todos los números del 1 al N.
-
-int num = int.Parse(Console.ReadLine());
-
-for (int i = 1; i <= num; i++) 
-{
-    Console.WriteLine(i);
-}
